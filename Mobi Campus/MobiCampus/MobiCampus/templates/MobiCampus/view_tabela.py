80a04c61a7cf79f0f865dff665b0394303e8eaf4
ABC <108697071+4BC5@users.noreply.github.com>
2022-09-25 00:10:01 -0300

    Add files via upload

---
+++ Mobi Campus/MobiCampus/MobiCampus/templates/MobiCampus/view_tabela.py
@@ -0,0 +1,7 @@
+def view_info(request): #base de view da tabela html
+	if request.method == 'POST'
+		#if pro botão 1
+		#if pro botão 2
+	else
+		objs=Model_name.objects.all()
+		return render(request,'template_name',{'objs':obj}) #essa é a view que preenche a tabela
